0c76014e5e041dd4d7a774b9518918eb987f64dd
Eronheit <aquinoeronaldo@gmail.com>
2021-10-19 22:42:48 -0300

    chore: config socket and cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import 'dotenv/config'
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 
 import { router } from './routes';
 
 const app = express();
+
+app.use(cors())
+
+const serverHttp = http.createServer(app)
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*'
+  }
+})
+
+io.on("connection", socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+})
 
 app.use(express.json())
 
@@ -21,4 +38,4 @@ app.get('/signin/callback', (request, response) => {
   return response.json(code);
 })
 
-app.listen(4000, () => console.log('🚀🚀 Server listen on port 4000'))
+serverHttp.listen(4000, () => console.log('🚀🚀 Server listen on port 4000'))
